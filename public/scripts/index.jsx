6cd2b2466af39ab65ae1e2e86bc85dea08f77a96
Architektor <gloosx@gmail.com>
2016-01-24 06:11:27 +0300

    fixed status change

--- public/scripts/index.jsx
+++ public/scripts/index.jsx
@@ -8,6 +8,12 @@ var Colors = require('material-ui/lib/styles/colors');
 var AppBar = require('material-ui/lib/app-bar');
 var TextField = require('material-ui/lib/text-field');
 var RaisedButton = require('material-ui/lib/raised-button');
+var FlatButton = require('material-ui/lib/flat-button');
+var IconButton = require('material-ui/lib/icon-button');
+var MenuItem = require('material-ui/lib/menus/menu-item');
+var Divider = require('material-ui/lib/divider');
+var IconMenu = require('material-ui/lib/menus/icon-menu');
+var DropDownMenu = require('material-ui/lib/DropDownMenu');
 
 var circleButtonStyle = {
 	height: 50,
@@ -73,7 +79,7 @@ var TaskBox = React.createClass({
 			url: this.props.url, 
 			dataType: 'json',
 			type: 'POST',
-			data: {"method": "append", "status": 1, "text": "New task"},
+			data: {"method": "append", "status": "0", "text": "New task"},
 			success: function(data) {
 				this.setState({data: data});
 			}.bind(this),
@@ -114,9 +120,9 @@ var TaskBox = React.createClass({
 		return (
 			<Paper className="paperE" zDepth={5}>
 			<AppBar 
-				showMenuIconButton={false}
-				title="Just To-Do. No shit"
-				style={appBarsStyle}
+			showMenuIconButton={false}
+			title="Just To-Do. No shit"
+			style={appBarsStyle}
 			/>
 			<TaskList data={this.state.data} onDelete={this.handleDelete} onEdit={this.handleEdit} onMove={this.handleMove}/>
 			<AddNewTask onAdd={this.handleAdd}/>
@@ -155,44 +161,48 @@ var TaskElement = React.createClass({
 		return {
 			text : "",
 			id : 0,
-			status: {},
-			editing: false
+			status: "",
+			editing: false,
+			hidingHandle: {
+			}
 		}
 	},
-	componentDidMount: function() {
-		this.setState({
-			text: this.props.text,
-			id: this.props.id,
-			status: this.props.status,
-			editing: this.props.editing
-		});
-		this.updateStatus();
-	},
-	updateStatus: function() {
-		switch(this.props.status){
+	updateStatus: function(newStatus) {
+		switch(newStatus){
 			case "1":
 				this.setState({
 					style: {
 						backgroundColor: Colors.brown100
-					}
+					},
+					status: newStatus
 				})
 				break;
 			case "2":
 				this.setState({
 					style: {
 						backgroundColor: Colors.indigo100
-					}			
+					},
+					status: newStatus			
 				})
 				break;
+
 			case "3":
 				this.setState({
 					style: {
 						backgroundColor: Colors.deepPurple100
-					}			
+					},
+					status: newStatus	
 				})
 				break;
 		};
-
+	},
+	componentDidMount: function() {
+		this.setState({
+			text: this.props.text,
+			id: this.props.id,
+			editing: this.props.editing
+		});
+		this.updateStatus(this.props.status)
 	},
 	deleteTask: function() {
 		this.props.onDelete(this.state.id);
@@ -202,7 +212,7 @@ var TaskElement = React.createClass({
 			editing: true
 		});
 	},
-	editTask: function(e) {
+	editTask: function() {
 		this.setState({
 			editing: false,
 		});
@@ -213,85 +223,92 @@ var TaskElement = React.createClass({
 			text: e.target.value
 		})
 	},
-	setStatus: function(status) {
-		this.setState({
-			status: status
-		})
-		this.props.onEdit(this.state.id, this.state.text, this.state.status);
-		this.updateStatus()
-	},
 	moveTask: function(direction){
 		this.props.onMove(this.state.id, direction) //1 means move UP and 0 means move DOWN
 	},
+	mouseOver: function(){
+		this.setState({
+			hidingHandle: {
+			}
+		})
+	},
+	mouseOut: function(){
+		this.setState({
+			hidingHandle: {
+				display: none
+			}
+		})
+	},	
 	render: function() {
+
 		if(!this.state.editing){
-			return (
-				<div className="taskElement" style={this.state.style}>
+				return (
+					<div className="taskElement" style={this.state.style} onMouseEnter={this.mouseOver}>
 					<div className="textArea">
-						<h4>{this.state.text}</h4>
+					<h4>{this.state.text}</h4>
 					</div>
-				<div className="iconsArea">
+					<div className="iconsArea" style={this.state.hidingHandle}>
 					<i className="material-icons" onClick={this.toEditMode}>mode_edit</i>
 					<i className="material-icons" onClick={this.deleteTask}>delete</i>
 					<i className="material-icons" onClick={this.moveTask.bind(this, 0)}>keyboard_arrow_down</i>
 					<i className="material-icons" onClick={this.moveTask.bind(this, 1)}>keyboard_arrow_up</i>
-				</div> 
-				</div>		   
-			);
+					</div> 
+					</div>
+				);
 		}else{ /*think about adding fullWidth={true} property*/
-		return (
-			<div className="taskElement" style={this.state.style}>
+			return (
+				<div className="taskElement" style={this.state.style}>
 				<TextField 
-					onChange={this.textChange}
-					defaultValue={this.state.text}
-					underlineStyle={{borderColor:Colors.blueGrey600}}
-					underlineFocusStyle={{borderColor:Colors.red100}} 
-					multiLine={true}
-					errorStyle={{color:Colors.blueGrey500}}
-					errorText={"Press \"Enter\" to submit your task"}
-					onEnterKeyDown={this.editTask}
+				onChange={this.textChange}
+				defaultValue={this.state.text}
+				underlineStyle={{borderColor:Colors.blueGrey600}}
+				underlineFocusStyle={{borderColor:Colors.red100}} 
+				multiLine={true}
+				errorStyle={{color:Colors.blueGrey500}}
+				errorText={"Press \"Enter\" to submit your task"}
+				onEnterKeyDown={this.editTask}
 				/>
 				<br/>
-				<RaisedButton label="brown" backgroundColor={Colors.brown100} onClick={this.setStatus.bind(this, "1")}/>
-				<RaisedButton label="blue" backgroundColor={Colors.indigo100} onClick={this.setStatus.bind(this, "2")}/>
-				<RaisedButton label="purple" backgroundColor={Colors.deepPurple100} onClick={this.setStatus.bind(this, "3")}/>
-			</div>
-		);
+				<RaisedButton label="brown" backgroundColor={Colors.brown100} onClick={this.updateStatus.bind(this, "1")}/>
+				<RaisedButton label="blue" backgroundColor={Colors.indigo100} onClick={this.updateStatus.bind(this, "2")}/>
+				<RaisedButton label="purple" backgroundColor={Colors.deepPurple100} onClick={this.updateStatus.bind(this, "3")}/>
+				</div>
+			);
+			}
 		}
-	}
-});
+	});
 
-var AddNewTask = React.createClass({
-	getInitialState: function() {
-		return {currentButtonStyle:circleButtonStyle};
-	},
-	handleMouseEnter: function() {
-		this.setState({
-			currentButtonStyle:circleButtonStyleHover
-		});
-	},
-	handleMouseLeave: function() {
-		this.setState({
-			currentButtonStyle:circleButtonStyle
-		});
-	},
-	render: function() {
-		var currentButtonStyle = this.state.currentButtonStyle;
-		return (
-			<div className="addNewTask" style={appBarsStyle}>
-			<Paper style={currentButtonStyle} zDepth={3} className="circleButtonStyle" circle={true} 
+	var AddNewTask = React.createClass({
+		getInitialState: function() {
+			return {currentButtonStyle:circleButtonStyle};
+		},
+		handleMouseEnter: function() {
+			this.setState({
+				currentButtonStyle:circleButtonStyleHover
+			});
+		},
+		handleMouseLeave: function() {
+			this.setState({
+				currentButtonStyle:circleButtonStyle
+			});
+		},
+		render: function() {
+			var currentButtonStyle = this.state.currentButtonStyle;
+			return (
+				<div className="addNewTask" style={appBarsStyle}>
+				<Paper style={currentButtonStyle} zDepth={3} className="circleButtonStyle" circle={true} 
 				onClick={this.props.onAdd}
 				onMouseEnter={this.handleMouseEnter}
 				onMouseLeave={this.handleMouseLeave}>
 
 				<i className="material-icons" >add</i>
 				</Paper>
-			</div>
-		);
-	}
-});
+				</div>
+			);
+		}
+	});
 
-ReactDOM.render(
-	<TaskBox url='/api/tasks' fs={require('fs')}/>,
-		document.getElementById('content')
-);
+	ReactDOM.render(
+		<TaskBox url='/api/tasks' fs={require('fs')}/>,
+			document.getElementById('content')
+	);
